bd7e15b2680a96fa6774ee95b58236b20ed8f3e2
Yann <yann@gmail.com>
2026-04-01 10:40:39 +0700

    Sorting - Main Insertion sort

--- SortingMain22.java
+++ SortingMain22.java
@@ -5,6 +5,7 @@ public class SortingMain22 {
 
         Sorting22 dataurut1 = new Sorting22 (a, a.length);
         Sorting22 dataurut2 = new Sorting22 (b, b.length);
+        Sorting22 dataurut3 = new Sorting22 (a, a.length);
 
         System.out.println("Data awal 1");
         dataurut1.tampil();
@@ -17,5 +18,12 @@ public class SortingMain22 {
         dataurut2.selectionSort();
         System.out.println("Data sudah diurutkan dengan Selection Sort (ASC)");
         dataurut2.tampil();
+
+        System.out.println("Data awal 3");
+        dataurut3.tampil();
+        dataurut3.insertionSort();
+        System.out.println("Data sudah diurutkan dengan Insertion Sort (ASC)");
+        dataurut3.tampil();
+
     }
 }
