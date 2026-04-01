a7b592a4c0319a2f3bb43c8fb9016d442f9d51a5
Yann <yann@gmail.com>
2026-04-01 10:26:28 +0700

    Sorting - Bubble sort

---
+++ Sorting22.java
@@ -0,0 +1,30 @@
+public class Sorting22 {        
+    int [] data;
+    int jumData;
+
+ Sorting22 (int Data [], int jmlData) {
+    jumData = jmlData;
+    data = new int [jumData];
+    for (int i = 0; i < jumData; i++) {
+        data [i] = Data [i];
+    }
+ }
+ void bubbleSort() {
+    int temp= 0;
+    for (int i = 0; i < jumData - 1; i++) {
+        for (int j = 1; j < jumData - i; j++) {
+            if (data [j-1] > data [j]) {
+                temp = data [j];
+                data [j] = data [j - 1];
+                data [j - 1] = temp;
+            }
+        }
+    }
+ }
+  void tampil () {
+    for (int i = 0; i < jumData; i++) {
+        System.out.print(data [i] + " ");
+    }
+    System.out.println();
+    }
+}
