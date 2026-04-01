2fccedee11a65e79dc6bcf5868b9307596e4c10a
Yann <yann@gmail.com>
2026-04-01 10:35:16 +0700

    Sorting - Main Selection Sort

--- SortingMain22.java
+++ SortingMain22.java
@@ -1,13 +1,21 @@
 public class SortingMain22 {
     public static void main(String[] args) {
         int a[] = {20, 10, 2, 7, 12};
+        int b[] = {30, 20, 2, 8, 14};
 
         Sorting22 dataurut1 = new Sorting22 (a, a.length);
+        Sorting22 dataurut2 = new Sorting22 (b, b.length);
 
         System.out.println("Data awal 1");
         dataurut1.tampil();
         dataurut1.bubbleSort();
         System.out.println("Data sudah diurutkan dengan Bubble Sort (ASC)");
         dataurut1.tampil();
+
+        System.out.println("Data awal 2");
+        dataurut2.tampil();
+        dataurut2.selectionSort();
+        System.out.println("Data sudah diurutkan dengan Selection Sort (ASC)");
+        dataurut2.tampil();
     }
 }
